9b2818a66f9e5085fb8c6ea092ecf2af8575a2d6
JCRamz <JCRamz1992@gmail.com>
2021-02-23 23:36:31 -0600

    Implemented artist search

--- app.py
+++ app.py
@@ -316,14 +316,33 @@ def search_artists():
     # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
     # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
     # search for "band" should return "The Wild Sax Band".
-    response={
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
+
+    searchQuery = request.form.get ('search_term')
+    query = db.session.query (Artist).filter (Artist.name.ilike (f'%{searchQuery}%')).all ()
+
+    data = []
+
+    for artist in query:
+        data.append ({
+            "id": artist.id,
+            "name": artist.name,
+            "num_upcoming_shows": len(db.session.query(Shows).filter(Shows.venues_id == artist.id).filter(
+                Shows.start_time > datetime.now()).all()),
+
+        })
+    response = {
+        "count": len(query),
+        "data": data
     }
+
+    # response={
+    #     "count": 1,
+    #     "data": [{
+    #         "id": 4,
+    #         "name": "Guns N Petals",
+    #         "num_upcoming_shows": 0,
+    #     }]
+    # }
     return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')

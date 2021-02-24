ca370a392cf49f668cc89cb257be5106043e6759
JCRamz <JCRamz1992@gmail.com>
2021-02-23 19:36:37 -0600

    Create show route successfully inserts data into our database.

--- app.py
+++ app.py
@@ -528,13 +528,26 @@ def create_shows():
 def create_show_submission():
     # called to create new shows in the db, upon submitting new show listing form
     # TODO: insert form data as a new Show record in the db, instead
+    try:
+        form = request.form
+        show = Shows(artist_id=form['artist_id'], venues_id=form['venue_id'], start_time=form['start_time'])
+        db.session.add(show)
+        db.session.commit()
 
-    # on successful db insert, flash success
-    flash('Show was successfully listed!')
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Show could not be listed.')
-    # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
-    return render_template('pages/home.html')
+        # on successful db insert, flash success
+        flash('Show was successfully listed!')
+
+    except:
+        # TODO: on unsuccessful db insert, flash an error instead.
+        # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+        flash('An error occurred. Show could not be listed.')
+        db.session.rollback()
+        print (sys.exc_info())
+
+    finally:
+        db.session.close()
+
+    return render_template ('pages/home.html')
 
 @app.errorhandler(404)
 def not_found_error(error):
